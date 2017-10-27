79c53b5eece47750c06aacc1d50fb44a3f95ed4b
Tejas Shah <tejas.s@browserstack.com>
2017-10-27 19:35:51 +0530

    Add App Automate tests

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # node-appium-app-browserstack
+App Automate Node Samples
+---------------------
+
+This repository contains code for Automated Native App tests. Please feel free to clone the repo and use the example code.
+
+For frameworks integration with BrowserStack, refer to their individual repositories -
+
+- [WebdriverIO](https://github.com/browserstack/webdriverio-appium-app-browserstack)
---
+++ ios/singleTest.js
@@ -0,0 +1,57 @@
+var wd = require('wd');
+var assert = require('assert');
+var asserters = wd.asserters;
+var sleep = require('sleep');
+var Q = wd.Q;
+
+desiredCaps = {
+  'browserstack.user' : 'BROWSERSTACK_USERNAME',
+  'browserstack.key' : 'BROWSERSTACK_ACCESS_KEY',
+  'build' : 'Node iOS',
+  'name': 'single_test',
+  'device' : 'iPhone 7 Plus',
+  'app' : 'bs://<hashed app-id>',
+  'browserstack.debug' : true,
+  'realMobile' : true
+};
+driver = wd.promiseRemote("http://hub.browserstack.com/wd/hub");
+
+driver
+  .init(desiredCaps)
+  .then(function () {
+    return driver.waitForElementById('Log In', asserters.isDisplayed && asserters.isEnabled, 30000);
+  })
+  .then(function (loginButton) {
+    return loginButton.click();
+  })
+  .then(function () {
+    return driver.waitForElementById('Email address', asserters.isDisplayed && asserters.isEnabled, 30000);
+  })
+  .then(function (emailInput) {
+    return emailInput.sendKeys("hello@browserstack.com");
+  })
+  .then(function () {
+    return driver.waitForElementById('Next', asserters.isDisplayed, 30000);
+  })
+  .then(function (nextButton) {
+    return nextButton.click().then(function() {
+      sleep.sleep(5);
+    });
+  })
+  .then(function() {
+    return driver.waitForElementsByXPath("//XCUIElementTypeStaticText", asserters.isDisplayed, 30000);
+  })
+  .then(function (textElements) {
+    return Q().then(function () {
+      return textElements.map(function (textElement) {
+        return textElement.text().then(function(value) {
+          if (value.indexOf('not registered') !== -1) {
+            console.log(value);
+            assert(value.indexOf('This email address is not registered on WordPress.com') !== -1);
+          }
+        });
+      })
+    }).all()
+  })
+  .fin(function() { return driver.quit(); })
+  .done();

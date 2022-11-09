d35b712e26f4b2b85df393d08dcf535f28736562
IanFan21 <77768236+IanFan21@users.noreply.github.com>
2022-11-09 14:52:42 +0700

    Update BrowserStackSample.js

--- android/BrowserStackSample.js
+++ android/BrowserStackSample.js
@@ -1,16 +1,15 @@
 let wd = require('wd');
 let assert = require('assert');
 let asserters = wd.asserters;
-let Q = wd.Q;
 desiredCaps = {
   // Set your BrowserStack access credentials
   'browserstack.user' : 'bayurizkhy_snQWko',
   'browserstack.key' : 'WZ4xpkZvnNREsUkUH8Wt',
-    // Set app_url of the application under test
+  // Set app_url of the application under test
   'app' : 'bs://df7671264546324fac5df2c770de1cfb6a3c1418',
   // Specify device and os_version for testing
-  'device' : 'Samsung Galaxy A10',
-  'os_version' : '9.0',
+  'device' : 'Samsung Galaxy A11',
+  'os_version' : '10.0',
   // Set other BrowserStack capabilities
   'project' : 'First NodeJS project',
   'build' : 'browserstack-build-1',
@@ -19,10 +18,31 @@ desiredCaps = {
 // Initialize the remote Webdriver using BrowserStack remote URL
 // and desired capabilities defined above
 driver = wd.promiseRemote("http://hub-cloud.browserstack.com/wd/hub");
-// Test case for the BrowserStack sample iOS app. 
-// If you have uploaded your app, update the test case here.
+// Test case for the BrowserStack sample Android app. 
+// If you have uploaded your app, update the test case here. 
 driver.init(desiredCaps)
-  //Write your custom code here
+  .then(function () {
+    return driver.waitForElementByAccessibilityId(
+      'Search Wikipedia', asserters.isDisplayed 
+      && asserters.isEnabled, 30000);
+  })
+  .then(function (searchElement) {
+    return searchElement.click();
+  })
+  .then(function () {
+    return driver.waitForElementById(
+      'org.wikipedia.alpha:id/search_src_text', asserters.isDisplayed 
+      && asserters.isEnabled, 30000);
+  })
+  .then(function (searchInput) {
+    return searchInput.sendKeys("BrowserStack");
+  })
+  .then(function () {
+    return driver.elementsByClassName('android.widget.TextView');    
+  })
+  .then(function (search_results) {
+    assert(search_results.length > 0);
+  })
   .fin(function() { 
     // Invoke driver.quit() after the test is done to indicate that the test is completed.
     return driver.quit(); 

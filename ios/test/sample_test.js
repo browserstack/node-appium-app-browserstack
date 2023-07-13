6be0e78d38d1828fc749fb2d58595fffaa4c9749
Neha Agarwal <agarneha1331@gmail.com>
2023-07-13 11:20:04 +0530

    Integrate with sdk

---
+++ ios/test/sample_test.js
@@ -0,0 +1,53 @@
+var assert = require('assert');
+const { Builder, By, until } = require("selenium-webdriver");
+
+var buildDriver = function() {
+  return new Builder()
+    .usingServer('http://127.0.0.1:4723/wd/hub')
+    .build();
+};
+
+async function bstackSampleTest () {
+  let driver =  buildDriver();
+  try {
+    await driver.wait(
+      until.elementLocated(
+        By.xpath(
+          '/XCUIElementTypeApplication/XCUIElementTypeWindow/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeButton[1]'
+        )
+      ), 3000
+    ).click();
+
+    var textInput = await driver.wait(
+      until.elementLocated(
+        By.xpath(
+          '/XCUIElementTypeApplication/XCUIElementTypeWindow[1]/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeTextField'
+        ), 3000
+      )
+    );
+    await textInput.sendKeys('hello@browserstack.com\n');
+    await driver.sleep(5000);
+
+    var textOutput = await driver.findElement(
+      By.xpath(
+        '/XCUIElementTypeApplication/XCUIElementTypeWindow[1]/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeStaticText'
+      )
+    ).getText();
+
+    assert(textOutput === 'hello@browserstack.com');
+    await driver.executeScript(
+      'browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"passed","reason": "Input and output text matches"}}'
+    );
+  } catch (e) {
+    console.log(e);
+    await driver.executeScript(
+      'browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"failed","reason": "Some elements failed to load"}}'
+    );
+  } finally {
+    if (driver) {
+      await driver.quit();
+    }
+  }
+}
+
+bstackSampleTest();
